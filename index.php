6f7ad4d7c7ca44eec6c4f367e6469d7335e58bb4
cristopherHurtado <cristopherhurtado@gmail.com>
2022-08-19 15:43:53 -0400

    ejercicio matriz

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    $valor = 5;
+     
+    $figura = array('x___x','_x_x_','__x__','_x_x_','x___x');
+
+    for($i=0; $i < $valor; $i++){
+       echo"$figura[i]";
+        for ($j=0; $j < $valor; $j++){
+            
+            echo"figura[j]";
+       };
+       
+       
+   };
+   
+   
+ 
+
+
+
+
+
+
+?>
